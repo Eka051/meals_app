6906d66bb6da313dd9f753c73579b3081c384c5c
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-24 20:39:39 +0700

    feat: implement category selection navigation to meals screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/screens/meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => MealsScreen(
+          title: 'Some Meals',
+          meals: [],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +33,9 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () => _selectCategory(context)),
         ],
       ),
     );
