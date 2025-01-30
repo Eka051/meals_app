8ac93f706df86da8fd4d3e234d6ed3a1d3b95955
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-30 22:12:04 +0700

    fix: ensure child widget is rendered

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -80,6 +80,7 @@ class _CategoriesScreenState extends State<CategoriesScreen>
           end: const Offset(0, 0),
         ).animate(CurvedAnimation(
             parent: _animationController, curve: Curves.easeInOut)),
+        child: child,
       ),
     );
   }
